275a76b6c2b4cfbf6be691ea7f527c5120d0fbd1
a-n-t-i-b-a-r-y-o-n <77591737+a-n-t-i-b-a-r-y-o-n@users.noreply.github.com>
2017-03-12 23:48:41 -0500

    Center "No Artwork\!" message properly-ish

--- mpdart.sh
+++ mpdart.sh
@@ -8,22 +8,23 @@ artfile="$HOME""/.ncmpcpp/.artwork.jpg"     # Feel free to change this value
 ffmpeg -y -hide_banner -v -8 -i "${filestr}" "$artfile"
 if [ $? -eq 1 ]; then
     l1=$(tput lines)
-    let l2=$l1/2
+    let l2=("$l1"/2)-1
     tput clear 
-    while [ $l1 -gt 0 ]; do 
-        printf "\n"
-        let l1=$l1-1
-    done
-    printf "\t\t┏━━━━━━━━━━━━┓\n"
-    printf "\t\t┃ No Artwork!┃\n"
-    printf "\t\t┗━━━━━━━━━━━━┛\n"
-    while [ $l2 -gt 0 ]; do
-        printf "\n"
-        let l2=$l2-1
-    done
+    coloffset=$(tput cols)/2
+    let coloffset=$coloffset-7  # The message box is 14 cols wide, so...
+    printf "%0.s\n" $(seq 1 "$l1")  #Kinda hacky but it works
+    if [ $coloffset -gt 0 ]; then
+        printf "%0.s " $(seq 1 $coloffset)
+        printf "┏━━━━━━━━━━━━┓\n"
+        printf "%0.s " $(seq 1 $coloffset)
+        printf "┃ No Artwork!┃\n"
+        printf "%0.s " $(seq 1 $coloffset)
+        printf "┗━━━━━━━━━━━━┛\n"
+    fi
+    printf "%0.s\n" $(seq 1 "$l2")
     exit 1
 fi
 tput clear
 sleep 1     # Don't wanna create a race condition
 imgcat "$artfile"
-tput cup -5,$(tput cols)
+tput cup 0,0
