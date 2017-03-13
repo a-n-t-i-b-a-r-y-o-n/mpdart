f439107bf70942759bed0b7e2b6746ca10bf4c9a
a-n-t-i-b-a-r-y-o-n <77591737+a-n-t-i-b-a-r-y-o-n@users.noreply.github.com>
2017-03-13 00:08:38 -0500

    Tiny code cleanup

--- mpdart.sh
+++ mpdart.sh
@@ -2,7 +2,7 @@
 
 read -r libprefix < "$HOME""/.mpdconf"
 filename=$(mpc -f "%file%" | head -n 1)
-#libprefix=${libprefix:25}       # This assumes your ~/.mpdconf stays the same
+libprefix=${libprefix:25}       # This assumes your ~/.mpdconf stays the same
 filestr=${libprefix%$'\"'}$filename
 artfile="$HOME""/.ncmpcpp/.artwork.jpg"     # Feel free to change this value
 ffmpeg -y -hide_banner -v -8 -i "${filestr}" "$artfile"
@@ -26,4 +26,4 @@ if [ $? -eq 1 ]; then
 fi
 tput clear
 imgcat "$artfile"
-#tput cup 0,0
+tput cup 0,0
